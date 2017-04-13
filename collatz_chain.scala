fb8d7ba63fddc5bb2bf3bcb09f9ae7f93fcbd1cc
FrogBomb <TLBlanchet@gmail.com>
2017-04-12 23:49:28 -0600

    Now with a cool union type!
    Made IntSingleOrList class to generate a union type as input to
    collatzChain for fun functional programing!

--- collatz_chain.scala
+++ collatz_chain.scala
@@ -1,5 +1,10 @@
 import scala.language.postfixOps
 
+class IntSingleOrList[T]
+object IntSingleOrList {
+  implicit object SingleWitness extends IntSingleOrList[Int]
+  implicit object ListWitness extends IntSingleOrList[List[Int]]
+}
 object Collatz {
   val halveEvens: PartialFunction[Int, Int] = new PartialFunction[Int, Int]{
     def isDefinedAt(x: Int) = x%2 == 0
@@ -8,18 +13,18 @@ object Collatz {
   def tripplePlusOne(x: Int) = 3*x + 1
   val colatzStep = halveEvens orElse PartialFunction[Int, Int](tripplePlusOne _)
 
-  def colatzChain(xs: List[Int]) : List[Int] = {
-    return xs match {
-        case 1 :: tail => 1 :: tail
-        case head :: _ => colatzChain(colatzStep(head) :: xs)
-        case _ => List()
+    return start match {
+        case x: Int => colatzChain(List(x))
+        case xs: List[Int] => xs match {
+          case 1 :: tail => 1 :: tail
+          case head :: _ => colatzChain(colatzStep(head) :: xs)
+          case _ => List[Int]()
+        }
       }
   }
 
   def main(args: Array[String]){
     args match {
-      case Array(h, _*) => println(colatzChain(List(h toInt)))
-      case _ => println(colatzChain(List(27)))
     }
   }
 }
