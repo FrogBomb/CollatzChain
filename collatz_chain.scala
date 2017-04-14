ea9d6430fd6cf176ee3937051b76b5338cefe395
FrogBomb <TLBlanchet@gmail.com>
2017-04-14 11:59:58 -0600

    More OO, less psudo union types
    I'm no longer pretending that scala has TRO and union types.

--- collatz_chain.scala
+++ collatz_chain.scala
@@ -1,11 +1,13 @@
 import scala.language.postfixOps
 
-class IntSingleOrList[T]
-object IntSingleOrList {
-  implicit object SingleWitness extends IntSingleOrList[Int]
-  implicit object ListWitness extends IntSingleOrList[List[Int]]
-}
-object Collatz {
+// class IntSingleOrList[T]
+// object IntSingleOrList {
+//   implicit object SingleWitness extends IntSingleOrList[Int]
+//   implicit object ListWitness extends IntSingleOrList[List[Int]]
+// }
+
+class CollatzChain(val start: Int) extends Iterator[Int] {
+  var current = start
   val halveEvens: PartialFunction[Int, Int] = new PartialFunction[Int, Int]{
     def isDefinedAt(x: Int) = x%2 == 0
     def apply(x: Int) = x / 2
@@ -13,21 +15,31 @@ object Collatz {
   def tripplePlusOne(x: Int) = 3*x + 1
   val collatzStep = halveEvens orElse PartialFunction[Int, Int](tripplePlusOne _)
 
-  def collatzChain[T: IntSingleOrList](start: T) : List[Int] = {
-    return start match {
-        case x: Int => collatzChain(List(x))
-        case xs: List[Int] => xs match {
-          case 1 :: tail => 1 :: tail
-          case head :: _ => collatzChain(collatzStep(head) :: xs)
-          case _ => List[Int]()
-        }
-      }
-  }
+  // def old_collatzChain[T: IntSingleOrList](start: T) : List[Int] = {
+  //   return start match {
+  //       case x: Int => old_collatzChain(List(x))
+  //       case xs: List[Int] => xs match {
+  //         case 1 :: tail => 1 :: tail
+  //         case head :: _ => old_collatzChain(collatzStep(head) :: xs)
+  //         case _ => List[Int]()
+  //       }
+  //     }
+  // }
 
+  def hasNext() : Boolean = {
+    return !(-1 to 1 contains current)
+  }
+  def next() : Int = {
+    current = collatzStep(current)
+    return current
+  }
+}
+
+object Collatz_main {
   def main(args: Array[String]){
     args match {
-      case Array(h, _*) => println(collatzChain(h toInt))
-      case _ => println(collatzChain(27))
+      case Array(h, _*) => println((new CollatzChain(h toInt)).toList)
+      case _ => println((new CollatzChain(27)).toList)
     }
   }
 }
